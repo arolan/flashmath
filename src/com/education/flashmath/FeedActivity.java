ff6f8c25689823c65659d00cf478f169436b8200
Huu Nguyen <huu@prismskylabs.com>
2013-10-30 20:27:52 -0700

    Get foreman to work

--- src/com/education/flashmath/FeedActivity.java
+++ src/com/education/flashmath/FeedActivity.java
@@ -3,6 +3,7 @@ package com.education.flashmath;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.TextView;
 
 public class FeedActivity extends Activity {
 
@@ -10,12 +11,26 @@ public class FeedActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_feed);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction ft =
+                getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flContainer, new DemoFragment());
+            ft.commit();
+        }
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.feed, menu);
+        TextView tvText = (TextView) findViewById(R.id.tvText);
+        EditText etView = (EditText) findViewById(R.id.etView);
+        Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        ListView lvTweets = (ListView) findViewById(R.id.lvTweets);
+
+
+        getFragmentManager();
 		return true;
 	}
 
