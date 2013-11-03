cb9ce0ab45df86c71250991fc49aa60c0323e538
Joohee Yoo <joohee.yoo@gmail.com>
2013-11-03 12:35:52 -0800

    subject Activity

---
+++ src/com/education/flashmath/SubjectActivity.java
@@ -0,0 +1,33 @@
+package com.education.flashmath;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class SubjectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_subject);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.subject, menu);
+		return true;
+	}
+	
+	public void onButtonClick(View v){
+		Intent i = new Intent(this, QuestionActivity.class);
+		String tag = v.getTag().toString();
+		i.putExtra("tag", tag);
+		Log.d("DEBUG",tag);
+		startActivity(i);
+	}
+
+}
