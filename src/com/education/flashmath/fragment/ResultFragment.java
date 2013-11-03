676acb7db3c82d9f95141a52f775a1a603fb20de
Huu Nguyen <huu@prismskylabs.com>
2013-11-03 00:33:31 -0700

    Add in working result activity and fragment

--- src/com/education/flashmath/fragment/ResultFragment.java
+++ src/com/education/flashmath/fragment/ResultFragment.java
@@ -9,20 +9,26 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.education.flashmath.R;
+import com.education.flashmath.ResultActivity;
 
 
 public class ResultFragment extends android.support.v4.app.Fragment {
+	private TextView tvScore;
+	private TextView tvRank;
+	
 	public View onCreateView(LayoutInflater inf, ViewGroup parent, Bundle savedInstanceState ){
-		return inf.inflate(R.layout.fragment_result, parent, false);	
-		
+		View view = inf.inflate(R.layout.fragment_result, parent, false);	
+		tvScore = (TextView) view.findViewById(R.id.tvScore);
+		tvRank = (TextView) view.findViewById(R.id.tvRank);
+		Button btnAnswer = (Button) view.findViewById(R.id.btnAnswer);
+		Button btnTryAgain = (Button) view.findViewById(R.id.btnTryAgain);
+		return view;
 	}
-
+	
 	public void onActivityCreated(Bundle savedInstanceState){
 		super.onActivityCreated(savedInstanceState);
-		TextView tvScore = (TextView)getActivity().findViewById(R.id.tvScore);
-		TextView tvRank = (TextView)getActivity().findViewById(R.id.tvRank);
-		Button btnAnswer = (Button)getActivity().findViewById(R.id.btnAnswer);
-		Button btnTryAgain = (Button)getActivity().findViewById(R.id.btnTryAgain);
+		tvScore.setText(((ResultActivity)getActivity()).getScore()+" / "+((ResultActivity)getActivity()).getSize());
+		tvRank.setText("1st");
 		
 	}
 }
