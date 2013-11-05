636bd5e76cbfbee5ff6ba1c45f2853b1337e1b41
Joohee Yoo <joohee.yoo@gmail.com>
2013-11-05 00:04:19 -0800

    Graph line and title

--- src/com/education/flashmath/LongActivity.java
+++ src/com/education/flashmath/LongActivity.java
@@ -1,15 +1,32 @@
 package com.education.flashmath;
 
+import android.graphics.Color;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class LongActivity extends Activity {
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
+import com.jjoe64.graphview.GraphViewStyle;
+import com.jjoe64.graphview.LineGraphView;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+public class LongActivity extends Activity {
+	LinearLayout llStats;
+	String subject;
+	String subjectTitle;
+	GraphViewData[] data;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,23 +34,64 @@ public class LongActivity extends Activity {
 		TextView tvSubject = (TextView) findViewById(R.id.tvSubject);
 		TextView tvLink = (TextView) findViewById(R.id.tvLink);
 	    TextView tvStudy = (TextView) findViewById(R.id.tvStudy);
+	    llStats = (LinearLayout) findViewById(R.id.llstats);
 		
-		String subject = getIntent().getStringExtra("subject");
-		String SubjectTitle = Character.toUpperCase(subject.charAt(0))+subject.substring(1);
+		subject = getIntent().getStringExtra("subject");
+		subjectTitle = Character.toUpperCase(subject.charAt(0))+subject.substring(1);
 		
-		tvSubject.setText("Your Progress of "+SubjectTitle);
-		tvStudy.setText("Study "+SubjectTitle);
+		tvSubject.setText(" Score History for "+ subjectTitle + " ");
+		tvSubject.setBackgroundColor(getColor());
+		tvSubject.setTextColor(Color.WHITE);
+		tvStudy.setText("Study "+subjectTitle);
 		
 		tvLink.setText(Html.fromHtml("<a href=http://en.wikipedia.org/wiki/"+subject+"_(mathematics)> GO TO WIKI "));
 		tvLink.setMovementMethod(LinkMovementMethod.getInstance());
+		
+		//Graph section
+				AsyncHttpClient client = new AsyncHttpClient();
+				client.get("http://flashmathapi.herokuapp.com/scores/" + subject + "/",
+						new JsonHttpResponseHandler() {
+					@Override
+					public void onSuccess(JSONArray jsonScores) {
+						data = new GraphViewData[jsonScores.length()];
+						for (int i = 0; i < jsonScores.length(); i++) {
+							try {
+								data[i] = (new GraphViewData(i + 1, jsonScores.getJSONObject(i).getInt("value")));
+							} catch (JSONException e) {
+								e.printStackTrace();
+							}
+						}
+						GraphView graphView = new LineGraphView(LongActivity.this,"");
+						GraphViewStyle style = new GraphViewStyle();
+						style.setVerticalLabelsColor(Color.BLACK);
+						style.setHorizontalLabelsColor(Color.BLACK);
+						style.setGridColor(Color.GRAY);
+						
+						GraphViewSeriesStyle lineStyle = new GraphViewSeriesStyle(getColor(), 5);
+						GraphViewSeries userData = new GraphViewSeries("Score", lineStyle, data);  
+						graphView.addSeries(userData);
+						graphView.setGraphViewStyle(style);
+						llStats.addView(graphView); 
+					}
+				});
 	}
 
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.long_menu, menu);
-        return true;
-    }
+
+	public int getColor(){
+		int color = 0;
+		if(subject.equals("addition")){
+			color = Color.parseColor("#2C8EB8");
+		} else if(subject.equals("subtraction")){
+			color = Color.parseColor("#D79BFA");
+		} else if(subject.equals("multiplication")){
+			color = Color.parseColor("#32D426");
+		} else if(subject.equals("fractions")){
+			color = Color.parseColor("#FA96D2");
+		} else if(subject.equals("division")){
+			color = Color.parseColor("#FFF126");
+		}
+		return color;
+	}
 	
 	//Close button
 	public void onClose(View v){
@@ -42,7 +100,7 @@ public class LongActivity extends Activity {
 	
 	//Clear button
 	public void onClear(View v){
-		
+	
 	}
 	
 }
