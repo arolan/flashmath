20657fc993bc0a573bd3afe19ca7b0f5c796a544
Huu Nguyen <huu@prismskylabs.com>
2013-11-10 16:56:57 -0800

    Polish up the einstein activity

--- src/com/education/flashmath/SubjectActivity.java
+++ src/com/education/flashmath/SubjectActivity.java
@@ -114,7 +114,7 @@ public class SubjectActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.subject, menu);
+		getMenuInflater().inflate(R.menu.subject, menu); 
 		getActionBar().setTitle("Flash Math");
 		return true;
 	}
