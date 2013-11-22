bc1e76944cd4d87f045fa22873c45cda426fdc3d
Huu Nguyen <huu@prismskylabs.com>
2013-11-21 16:32:40 -0800

    Hash the device code using SHA-1

--- src/com/education/flashmath/network/FlashMathClient.java
+++ src/com/education/flashmath/network/FlashMathClient.java
@@ -1,5 +1,10 @@
 package com.education.flashmath.network;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
 import android.content.Context;
 import android.provider.Settings.Secure;
 
@@ -17,7 +22,7 @@ public class FlashMathClient {
 		// TODO Replace this "global" with the device token
 		String android_id = Secure.getString(context.getContentResolver(),
                 Secure.ANDROID_ID); 
-		auth.put("token", android_id);
+		auth.put("token", encryptPassword(android_id));
 		client = new AsyncHttpClient();
 	}
 	
@@ -46,5 +51,38 @@ public class FlashMathClient {
 	public void putScore(String subject, String score, AsyncHttpResponseHandler handler) {
 		String url = "http://flashmathapi.herokuapp.com/scores/" + subject + "/" + score + "/";
     	client.get(url, auth, handler);
+	}
+	
+	private static String encryptPassword(String password)
+	{
+	    String sha1 = "";
+	    try
+	    {
+	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	        crypt.reset();
+	        crypt.update(password.getBytes("UTF-8"));
+	        sha1 = byteToHex(crypt.digest());
+	    }
+	    catch(NoSuchAlgorithmException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    catch(UnsupportedEncodingException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    return sha1;
+	}
+
+	private static String byteToHex(final byte[] hash)
+	{
+	    Formatter formatter = new Formatter();
+	    for (byte b : hash)
+	    {
+	        formatter.format("%02x", b);
+	    }
+	    String result = formatter.toString();
+	    formatter.close();
+	    return result;
 	}
 }
